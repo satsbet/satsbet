b4c4f324b747a73affc3bbcaf764cd1f0edb4b64
Wlad Paiva <desenvolvedormg@gmail.com>
2023-11-02 20:09:15 -0300

    add last quote UI

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,12 @@
 import { prisma } from "@/utils/prisma";
-import { checkInvoice, createInvoice } from "./api";
+import { useCurrencyFormatter } from "../hooks/use-currency-formatter";
 
 async function getAllBets() {
   return prisma.bet.findMany();
 }
-async function getAllQuotes() {
+
+/** Show the price from yesterday's btc */
+async function getLastQuote() {
   return prisma.quote.findFirst({
     orderBy: {
       day: "desc",
@@ -13,41 +15,12 @@ async function getAllQuotes() {
 }
 
 export default async function Home() {
-  const allBets = await getAllBets();
-  const allQuotes = await getAllQuotes();
-  // const users = await getUsers()
-  const walletData = await createInvoice(100, "Tiago's test");
-  const invoiceStatus = await checkInvoice(walletData.payment_hash);
+  const lastQuote = await getLastQuote();
+  const { format } = useCurrencyFormatter();
 
   return (
     <div className="text-red-50">
-      {/* <div className='users'>
-        <h1>Users:</h1>
-          {JSON.stringify(users)}
-          --------------------------------------
-      </div> */}
-
-      <div className="invoice-data">
-        <h1>Invoice:</h1>
-        {JSON.stringify(walletData)}
-        --------------------------------------
-      </div>
-
-      <div className="invoice-status">
-        <h1>Invoice Payment Status:</h1>
-        {JSON.stringify(invoiceStatus)}
-      </div>
-      {/* <div className="">
-        all bets:
-        {JSON.stringify(allBets.map(x => ({
-          ...x,
-          amount: Number(x.amount)
-        })), null, 2)}
-      </div>
-      <div className="">
-        all quotes:
-        {JSON.stringify(allQuotes, null, 2)}
-      </div> */}
+      Last quote: {format(lastQuote!.price / 100)}
     </div>
   );
 }

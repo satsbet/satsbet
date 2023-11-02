41bdca717350df0fee6524f1e9586378a5a0262a
Tiago Góes <tiago.goes2009@gmail.com>
2023-11-02 18:29:11 -0300

    Use env vars
    Signed-off-by: Tiago Góes <tiago.goes2009@gmail.com>

--- apps/web/utils/lnbits.ts
+++ apps/web/utils/lnbits.ts
@@ -1,7 +1,8 @@
-import LNBits from "lnbits"; // using import
+import LNBits from "lnbits";
+
 
 export const { wallet, userManager, paywall, withdraw, paylink, tpos } = LNBits({
-  adminKey: 'c1363046069c4ed2b91ab78d19eb1836',
-  invoiceReadKey: 'd9fdb259c8744fdc859d4f1fe25f7692',
-  endpoint: 'https://9a76e1ccb3.d.voltageapp.io',
+  adminKey: process.env.LN_ADMIN_KEY || "",
+  invoiceReadKey: process.env.LN_INVOICE_READ_KEY || "",
+  endpoint: process.env.LN_ENDPOINT || "",
 });

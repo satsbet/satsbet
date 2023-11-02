8eabc57fd3be6bb1dc1158a35860396bca14a58c
Wlad Paiva <desenvolvedormg@gmail.com>
2023-11-02 18:01:28 -0300

    move lnbits to env vars

---
+++ apps/web/utils/env.ts
@@ -0,0 +1,34 @@
+// src/env.mjs
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    DATABASE_URL: z.string().url(),
+    LNBITS_ENDPOINT: z.string().url(),
+    LNBITS_ADMIN_KEY: z.string(),
+    LNBITS_INVOICE_READ_KEY: z.string(),
+  },
+  /*
+   * Environment variables available on the client (and server).
+   *
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  client: {},
+  /*
+   * Due to how Next.js bundles environment variables on Edge and Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+   */
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    LNBITS_ENDPOINT: process.env.LNBITS_ENDPOINT,
+    LNBITS_ADMIN_KEY: process.env.LNBITS_ADMIN_KEY,
+    LNBITS_INVOICE_READ_KEY: process.env.LNBITS_INVOICE_READ_KEY,
+  },
+});

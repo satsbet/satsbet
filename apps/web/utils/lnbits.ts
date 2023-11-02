e6817207943415806183bf76b9ef0a48939fbf2d
Wlad Paiva <desenvolvedormg@gmail.com>
2023-11-02 17:38:44 -0300

    add lnbits

---
+++ apps/web/utils/lnbits.ts
@@ -0,0 +1,7 @@
+import LNBits from "lnbits"; // using import
+
+export const lnbits = LNBits({
+  adminKey: "d00265e7de5f44f59b2408d9f0564181",
+  invoiceReadKey: "23e34be59d57408688a74500a3f24f03",
+  endpoint: "https://lnbits.com", //default
+});

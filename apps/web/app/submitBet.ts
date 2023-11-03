9f9381dfe58784151454d5930c481fcfc1f96ea8
Tiago Góes <tiago.goes2009@gmail.com>
2023-11-02 22:36:15 -0300

    Save invoice payment hash after generating invoice on bet's model
    Signed-off-by: Tiago Góes <tiago.goes2009@gmail.com>

--- apps/web/app/submitBet.ts
+++ apps/web/app/submitBet.ts
@@ -11,17 +11,19 @@ export type SubmitBetInput = {
 export async function submitBet(bet: SubmitBetInput) {
   const today = new Date();
   today.setHours(0, 0, 0, 0); // Set to the start of the day
-  await prisma.bet.create({
-    data: {
-      ...bet,
-      status: BetStatus.PENDING,
-      createAt: today,
-    },
-  });
 
   const lnbitsResponse = await createInvoice(
     Number(bet.amount),
     "Thank you for betting with Satoshi!",
   );
+
+  await prisma.bet.create({
+    data: {
+      ...bet,
+      status: BetStatus.PENDING,
+      createAt: today,
+      paymentHash: lnbitsResponse.payment_hash,
+    },
+  });
   return lnbitsResponse.payment_request;
 }

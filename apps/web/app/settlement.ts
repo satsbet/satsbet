6da773467729bcfefc436ed3561ed304aa228a34
Rodrigo Vilar <rvilar@gmail.com>
2023-11-03 00:30:19 -0300

    [BE] Settlement logic

---
+++ apps/web/app/settlement.ts
@@ -0,0 +1,90 @@
+import { Bet, BetStatus, BetTarget, Quote } from "@prisma/client";
+import { calculateMultiplier } from "./multipliers";
+import { prisma } from "@/utils/prisma";
+import { payInvoice } from "./api";
+
+export async function createLnAddressInvoice(
+  lnAddress: string,
+  amount: number,
+) {
+  console.log(`Creating invoice for ${lnAddress} with amount ${amount}`);
+  return "invoice request"; //TODO: Implement
+}
+
+export async function runSettlement() {
+  const yesterdayQuote = await prisma.quote.findFirst({
+    orderBy: { day: "desc" },
+    skip: 1,
+  });
+  const todayQuote = await prisma.quote.findFirst({
+    orderBy: { day: "desc" },
+  });
+  const todayPaidBets = await prisma.bet.findMany({
+    where: { status: BetStatus.PAID },
+  });
+  if (!!yesterdayQuote && !!todayQuote && !!todayPaidBets.length) {
+    await processSettlement(yesterdayQuote, todayQuote, todayPaidBets);
+  }
+}
+
+export async function processSettlement(
+  yesterdayQuote: Quote,
+  todayQuote: Quote,
+  todayPaidBets: Bet[],
+) {
+  const betResult =
+    todayQuote.price > yesterdayQuote.price ? BetTarget.UP : BetTarget.DOWN;
+  const { amountUp, amountDown } = calculateAmounts(todayPaidBets);
+  let multiplier =
+    betResult === BetTarget.UP
+      ? calculateMultiplier(amountUp, amountDown)
+      : calculateMultiplier(amountDown, amountUp);
+
+  // Get bets based on the result
+  const betsToProcess = todayPaidBets.filter((bet) => bet.target === betResult);
+
+  // Create a promise for each bet and process them in parallel
+  const processBetsPromises = betsToProcess.map((bet) => {
+    const newStatus = betResult === bet.target ? BetStatus.WIN : BetStatus.LOST;
+    return processBet(newStatus, bet, multiplier); // assuming processBet is async and returns a Promise
+  });
+
+  // Wait for all the bet processing to complete
+  await Promise.all(processBetsPromises);
+}
+
+function calculateAmounts(todayPaidBets: Bet[]) {
+  return todayPaidBets.reduce(
+    (acc, bet) => {
+      if (bet.target === BetTarget.UP) {
+        acc.amountUp += bet.amount;
+      } else {
+        acc.amountDown += bet.amount;
+      }
+      return acc;
+    },
+    { amountUp: 0n, amountDown: 0n },
+  );
+}
+
+async function processBet(newStatus: BetStatus, bet: Bet, multiplier: number) {
+  const amountToPay =
+    newStatus === BetStatus.WIN ? Number(bet.amount) * multiplier : 0;
+  bet.status = newStatus;
+  updateBetStatus(bet.id, bet.status);
+  if (!!amountToPay) {
+    const paymentRequest = await createLnAddressInvoice(
+      bet.lnAddress,
+      amountToPay,
+    );
+    await payInvoice(paymentRequest);
+    await updateBetStatus(bet.id, BetStatus.REFUNDED);
+  }
+}
+
+async function updateBetStatus(betId: string, status: BetStatus) {
+  await prisma.bet.update({
+    where: { id: betId },
+    data: { status },
+  });
+}

4a800db0b6c5c6af4e3c1a6df27966de6ac8aa00
Tiago Góes <tiago.goes2009@gmail.com>
2023-11-03 11:25:38 -0300

    Invoke settlement after saving BTC price
    Signed-off-by: Tiago Góes <tiago.goes2009@gmail.com>

--- apps/web/app/api/cron/oracle/route.ts
+++ apps/web/app/api/cron/oracle/route.ts
@@ -1,7 +1,8 @@
 import { setBTCLastPrice } from "@/app/actions";
+import { GET } from "../settlement/route";
 
 export async function POST(request: Request) {
   const { price } = await request.json();
   await setBTCLastPrice(price);
-  return Response.json({ ok: true, price });
+  return await GET();
 }

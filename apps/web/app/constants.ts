3a4e8515c7ce03317e7ba8e104325b19761d27ce
Wlad Paiva <desenvolvedormg@gmail.com>
2023-11-03 09:10:04 -0300

    simplify multipler logic

--- apps/web/app/constants.ts
+++ apps/web/app/constants.ts
@@ -1,3 +1,3 @@
 export const SATSBET_FEE_PERCENT = 10;
-export const PENDING_BETS_WEIGHT_PERCENT = 30;
+export const PENDING_BETS_WEIGHT_PERCENT = 0.3;
 export const TOP_MULTIPLIER = 3;
--- apps/web/app/loader.spec.ts
+++ apps/web/app/loader.spec.ts
@@ -2,50 +2,57 @@ import { beforeEach, describe, expect, it, vi } from "vitest";
 import { TOP_MULTIPLIER } from "./constants";
 import { calculateMultiplier } from "./loader";
 
-describe("multiplier", () => {
-  beforeEach(() => {
-    // Reset the mocks before each test
-    vi.clearAllMocks();
-  });
+beforeEach(() => {
+  // Reset the mocks before each test
+  vi.clearAllMocks();
+});
 
-  describe("calculateMultiplier", () => {
-    it("should return TOP_MULTIPLIER if amount2 is zero", () => {
-      const multiplier = calculateMultiplier(100, 0);
-      expect(multiplier).toBe(TOP_MULTIPLIER);
-    });
+it("should return TOP_MULTIPLIER if amount2 is zero", () => {
+  const multiplier = calculateMultiplier(100, 0);
+  expect(multiplier).toBe(TOP_MULTIPLIER);
+});
 
-    it("should multiplier be a fraction", () => {
-      const amount1 = 100;
-      const amount2 = 50;
+it("should multiplier be a fraction", () => {
+  const amount1 = 100;
+  const amount2 = 50;
 
-      // 100 + 50 = 150 - 10% (fee) = 135 / 100 = 1.35
-      const expectedMultiplier = 1.35;
-      const multiplier = calculateMultiplier(amount1, amount2);
+  // 100 + 50 = 150 - 10% (fee) = 135 / 100 = 1.35
+  const expectedMultiplier = 1.35;
+  const multiplier = calculateMultiplier(amount1, amount2);
 
-      expect(multiplier).toBe(expectedMultiplier);
-    });
+  expect(multiplier).toBe(expectedMultiplier);
+});
 
-    it("should multiplier be > 2", () => {
-      const amount1 = 50;
-      const amount2 = 100;
+it("should multiplier be > 2", () => {
+  const amount1 = 50;
+  const amount2 = 100;
 
-      // 50 + 100 = 150 - 10% (fee) = 135 / 50 = 2.7
-      const expectedMultiplier = 2.7;
-      const multiplier = calculateMultiplier(amount1, amount2);
+  // 50 + 100 = 150 - 10% (fee) = 135 / 50 = 2.7
+  const expectedMultiplier = 2.7;
+  const multiplier = calculateMultiplier(amount1, amount2);
 
-      expect(multiplier).toBeLessThanOrEqual(TOP_MULTIPLIER);
-      expect(multiplier).toBe(expectedMultiplier);
-    });
+  expect(multiplier).toBeLessThanOrEqual(TOP_MULTIPLIER);
+  expect(multiplier).toBe(expectedMultiplier);
+});
 
-    it("should cap multiplier to TOP_MULTIPLIER", () => {
-      const amount1 = 10;
-      const amount2 = 100;
-      // 10 + 100 = 110 - 10% (fee) = 99 / 10 = 9.9 (should be capped to 3)
-      const expectedMultiplier = TOP_MULTIPLIER;
-      const multiplier = calculateMultiplier(amount1, amount2);
+it("should cap multiplier to TOP_MULTIPLIER", () => {
+  const amount1 = 10;
+  const amount2 = 100;
+  // 10 + 100 = 110 - 10% (fee) = 99 / 10 = 9.9 (should be capped to 3)
+  const expectedMultiplier = TOP_MULTIPLIER;
+  const multiplier = calculateMultiplier(amount1, amount2);
 
-      expect(multiplier).toBeLessThanOrEqual(TOP_MULTIPLIER);
-      expect(multiplier).toBe(expectedMultiplier);
-    });
-  });
+  expect(multiplier).toBeLessThanOrEqual(TOP_MULTIPLIER);
+  expect(multiplier).toBe(expectedMultiplier);
+});
+
+it.todo("should not have a multiplier smaller than 1", () => {
+  const amount1 = 11036.9;
+  const amount2 = 1050;
+  // 11036.9 + 1050 = 12086.9 - 10% (fee) = 10878.21 / 11036.9 = 0.98
+  const expectedMultiplier = TOP_MULTIPLIER;
+  const multiplier = calculateMultiplier(amount1, amount2);
+
+  expect(multiplier).toBeGreaterThan(1);
+  expect(multiplier).toBe(expectedMultiplier);
 });

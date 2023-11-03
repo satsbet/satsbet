64e91a69f7ddbf092a1bc6972194d958f551284c
Wlad Paiva <desenvolvedormg@gmail.com>
2023-11-03 12:58:40 -0300

    Revert "remove cache"
    This reverts commit f9db9781a26e5cfa793d812310cd1bca73afbb86.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,38 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: [],
-  async headers() {
-    return [
-      {
-        source: "/api/cron/payment",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, max-age=0",
-          },
-        ],
-      },
-      // TODO: maybe should be removed
-      {
-        source: "/api/cron/settlement",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, max-age=0",
-          },
-        ],
-      },
-      {
-        source: "/api/cron/oracle",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, max-age=0",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
